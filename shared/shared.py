9487d67fe171514a44946b75a0c0f4bd06e4e8bf
wangkun <git@wangkun.vip>
2023-07-26 14:29:51 +0800

    sd服务地址 添加配置项

--- shared/shared.py
+++ shared/shared.py
@@ -29,8 +29,11 @@ with open(".config/server_config.json", encoding="utf-8") as f:
     config = json.load(f)
 f.close()
 server_host = config["server_host"]
+sd_server_host = config["sd_server_host"]
 if "SERVER_HOST" in os.environ:
     server_host = os.environ["SERVER_HOST"]
+if "SD_SERVER_HOST" in os.environ:
+    sd_server_host = os.environ["SD_SERVER_HOST"]
 
 # general config
 with open(".config/config.json", encoding="utf-8") as f:
@@ -98,7 +101,7 @@ DESTROY_ALL = 9999
 OTHER_REQUEST = 10000
 
 # stable_diff config
-API_URL_v21 = "192.168.10.129:7860/queue/join"
+API_URL_v21 = sd_server_host + "/queue/join"
 API_URL_v15 = ""
 cache_dir = ".cache/"
 
